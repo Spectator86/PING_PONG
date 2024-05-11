f9756d05782aa0f403b24fec443a6e1bbfbd04f4
Spectator86 <167618825+Spectator86@users.noreply.github.com>
2024-05-11 15:16:44 +0300

    Update main.py

--- PING_PONG/main.py
+++ PING_PONG/main.py
@@ -86,20 +86,20 @@ while True:
         player2.rect.x += speed
 
     if Ball.rect.x + 30 >= W:
-        if speedY > 0 and speedY < limit:
-            speedY *= -1.1
-        elif speedY < 0 and speedY > -limit:
-            speedY *= -1.1
+        if speedX > 0 and speedX < limit:
+            speedX *= -1.1
+        elif speedX < 0 and speedX > -limit:
+            speedX *= -1.1
         else:
-            speedY *= -1
+            speedX *= -1
 
     elif Ball.rect.x <= 0:
-       if speedY > 0 and speedY < limit:
-            speedY *= -1.1
-        elif speedY < 0 and speedY > -limit:
-            speedY *= -1.1
+       if speedX > 0 and speedX < limit:
+            speedX *= -1.1
+        elif speedX < 0 and speedX > -limit:
+            speedX *= -1.1
         else:
-            speedY *= -1
+            speedX *= -1
 
     if pg.sprite.collide_rect(player1, Ball) or pg.sprite.collide_rect(player2, Ball):
         if speedY > 0 and speedY < limit:
